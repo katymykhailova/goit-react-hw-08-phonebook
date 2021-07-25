fef02f3e2a43ee4c122f3acaefc573709bee5efe
kotikm81 <77439323+kotikm81@users.noreply.github.com>
2021-07-26 00:55:35 +0300

    Удаляет комментарии

--- src/redux/contacts/contacts-reducer.js
+++ src/redux/contacts/contacts-reducer.js
@@ -1,6 +1,5 @@
 import { combineReducers } from 'redux';
 import { createReducer } from '@reduxjs/toolkit';
-// import actions from './contacts-actions';
 import { changeFilter } from './contacts-actions';
 
 import {
@@ -10,12 +9,8 @@ import {
 } from './contacts-operations';
 
 const items = createReducer([], {
-  // [fetchContactsSuccess]: (_, { payload }) => payload,
   [fetchContacts.fulfilled]: (_, { payload }) => payload,
-  // [addContactSuccess]: (state, { payload }) => [...state, payload],
   [addContact.fulfilled]: (state, { payload }) => [...state, payload],
-  // [deleteContactSuccess]: (state, { payload }) =>
-  //   state.filter(({ id }) => id !== payload),
   [deleteContact.fulfilled]: (state, { payload }) =>
     state.filter(({ id }) => id !== payload),
 });
